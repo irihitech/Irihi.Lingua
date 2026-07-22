cebc0ab52f275b07f5d572a61b483892d685612f
Dong Bin <popmessiah@hotmail.com>
2026-07-22 19:01:59 +0800

    feat: add XmlnsDefinition + XmlnsPrefix for Irihi.Luna.Lingua namespace
    - XmlnsDefinition maps https://irihi.tech/lingua -> Irihi.Luna.Lingua
    - XmlnsPrefix recommends 'lingua' prefix
    - Demo updated: xmlns:lingua="https://irihi.tech/lingua" replaces using: syntax

--- src/Irihi.Lingua.Core/Properties/AssemblyInfo.cs
+++ src/Irihi.Lingua.Core/Properties/AssemblyInfo.cs
@@ -2,4 +2,6 @@ using System.Runtime.CompilerServices;
 using Avalonia.Metadata;
 
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Irihi.Lingua.Extensions")]
+[assembly: XmlnsDefinition("https://irihi.tech/lingua", "Irihi.Luna.Lingua")]
+[assembly: XmlnsPrefix("https://irihi.tech/lingua", "lingua")]
 [assembly: InternalsVisibleTo("Irihi.Lingua.Core.Tests")]
